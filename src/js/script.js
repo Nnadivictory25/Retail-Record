fc885ed6e0f966189da182aed05db9ff2a2b2a46
Nnadi Victory <chikamagozi25@gmail.com>
2023-02-08 22:50:16 +0100

    fixed homepage animation not display, fixed vercel not using clean domains

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,37 @@
-const fadeEl = document.querySelector('.fade-in');
-const heroText = document.querySelector('.hero__text');
-const words = ['more efficient', 'faster', 'easier']
+const fadeEl = document.querySelector(".fade-in");
+const heroText = document.querySelector(".hero__text");
+const words = ["more efficient", "faster", "easier"];
 let count = 0;
 
 function changeWord() {
-    if (count === words.length) {
-        count = 0;
-    }
-    heroText.innerHTML = `
+  if (count === words.length) {
+    count = 0;
+  }
+  heroText.innerHTML = `
     Recording your physical store sales have never been <span class="fade-in">${words[count]}</span> with RetailRecord
     `;
-    count++;
+  count++;
 }
-if (fadeEl) {
-    setTimeout(() => {
-        setInterval(changeWord, 5000);
-    }, 2000);
+if (heroText) {
+  setTimeout(() => {
+    setInterval(changeWord, 5000);
+  }, 2000);
+}
+
+if (document.querySelector(".input")) {
+  const inputs = Array.from(document.querySelectorAll(".input"));
+  const btn = document.querySelector("button");
+  const form = document.querySelector("#form");
+
+  inputs.forEach((input) => {
+    input.addEventListener("keyup", validateInput);
+  });
+
+  const validateInput = () => {
+    if (inputs.some((input) => input.value == "")) {
+      btn.disabled = true;
+    } else {
+      btn.disabled = false;
+    }
+  }
 }
