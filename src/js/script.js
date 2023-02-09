bcf3c29fbc739682d7ab853059b3e5dea6a22273
Nnadi Victory <chikamagozi25@gmail.com>
2023-02-09 21:20:11 +0100

    used jquery for the form

--- src/js/script.js
+++ src/js/script.js
@@ -5,6 +5,8 @@ const btn = document.querySelector("button");
 const words = ["more efficient", "faster", "easier"];
 let count = 0;
 
+
+
 function changeWord() {
   if (count === words.length) {
     count = 0;
@@ -21,9 +23,7 @@ if (heroText) {
 }
 
 const validateInput = () => {
-  if (
-    inputs.some((input) => input.value == "")
-  ) {
+  if (inputs.some((input) => input.value == "")) {
     btn.disabled = true;
   } else {
     btn.disabled = false;
@@ -35,35 +35,49 @@ function checkRecaptcha() {
 }
 
 if (window.location.href.includes("contact")) {
+  let mainWidget;
+  var verifyCallback = function (response) {
+    alert(response);
+  };
+  var onloadCallback = function () {
+    mainWidget = grecaptcha.enterprise.render("recaptcha", {
+      sitekey: " 6LfzDUYkAAAAAExZEJBrJLkpVVdJjL2D6quRu0E9",
+    });
+  };
   const form = document.querySelector("#form");
+  let token;
 
   inputs.forEach((input) => {
     input.addEventListener("keyup", validateInput);
   });
 
   form.addEventListener("submit", (e) => {
-    grecaptcha.enterprise.ready(function () {
-      grecaptcha.enterprise
-        .execute("6LfzDUYkAAAAAExZEJBrJLkpVVdJjL2D6quRu0E9", {
-          action: "sendForm",
-        })
-        .then(function (token) {
-          console.log(token);
-          document.querySelector("#g-response").value = token;
-        });
-    });
     e.preventDefault();
+    token = grecaptcha.enterprise.getResponse(mainWidget);
+    console.log(token);
     btn.innerHTML = `Sending <i class="bi bi-send-fill"></i>`;
     btn.disabled = true;
-    emailjs.sendForm("service_0f19o8c", "template_m1ot4np", form).then(
-      function (response) {
-        console.log("SUCCESS!", response.status, response.text);
-      },
-      function (error) {
-        alert("Something went wrong! Please try again.", error);
-        // window.location.reload();
-        console.log("FAILED...", error);
+
+    var data = {
+      service_id: "service_0f19o8c",
+      template_id: "template_m1ot4np",
+      user_id: "BBHK4CoORgSaMhZIR",
+      template_params: {
+        'full-name': document.querySelector('#fullName').value,
+        'email': document.querySelector('#email').value,
+        'message': document.querySelector('#message').value,
+          'g-recaptcha-response': token
       }
-    );
+    };
+    
+    $.ajax('https://api.emailjs.com/api/v1.0/email/send', {
+      type: 'POST',
+      data: JSON.stringify(data),
+      contentType: 'application/json'
+    }).done(function() {
+      console.log('Your mail is sent!');
+    }).fail(function(error) {
+      console.log('Oops... ' + JSON.stringify(error));
+    });
   });
 }
