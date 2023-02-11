56171a001ec572d0831d42eb82a954304e469e8d
Nnadi Victory <chikamagozi25@gmail.com>
2023-02-11 15:47:15 +0100

    added timing functionality when a user confirms email

--- src/js/welcome.js
+++ src/js/welcome.js
@@ -2,12 +2,36 @@ const SUPABASE_URL = "https://plxvbnykwsgwglpxusar.supabase.co";
 const SUPABASE_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBseHZibnlrd3Nnd2dscHh1c2FyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzU5Nzk5OTYsImV4cCI6MTk5MTU1NTk5Nn0.4e6f--5uvLzay-Xduh0OIRF3fFK3AGHqdsAqMV6vUC0";
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+let time = 5
 
 
+const timerFunction = (el) => { 
+    if (time === 0) {
+        window.location.href = '/dashboard'
+    }
+
+    setInterval(() => {
+        time--
+    }, 1000);
+
+    return time;
+}
 
 const getUser = async () => {
+    const welcomeEl = document.querySelector('#welcome')
     const { data: { user } } = await supabase.auth.getUser()
     console.log(user)
-    return user
+
+    if (user) {
+        welcomeEl.innerHTML += `
+        <img class="w-56 mx-auto" src="/images/undraw_happy_news_re_tsbd.svg">
+
+        <h1 class="text-center">Email Verification Successful !</h1>
+
+        <p class="text-center text-lg">
+            You will redirected to your dashboard in <span class="timer font-medium text-[hsl(216,93%,44%)]">${timerFunction()}</span>
+        </p>
+        `
+    }
 }
 getUser()
